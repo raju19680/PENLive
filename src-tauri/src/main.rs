cde9a05d60f60e12412e41b033acb7c48a3d8cee
raju19680 <igsybegun@gmail.com>
2026-06-18 18:43:55 +0530

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,8 @@
 // PENLIVE - Main Entry Point
 // Copyright (c) 2026 Er. Raju Kumawat. All rights reserved.
 
-// Prevents additional console window on Windows in release
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    penlive_lib::run()
+    penlive::run()
 }
